db5f7fdbce838d09b604596fe52f205077b91093
PauGi <pau.giro@enti.cat>
2025-01-09 16:56:29 +0100

    Functional Api

---
+++ app/src/main/java/models/MarvelResponse.kt
@@ -0,0 +1,23 @@
+package models
+
+data class MarvelResponse(
+    val code: Int,
+    val status: String,
+    val data: MarvelData
+)
+
+data class MarvelData(
+    val results: List<MarvelCharacter>
+)
+
+data class MarvelCharacter(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val thumbnail: Thumbnail
+)
+
+data class Thumbnail(
+    val path: String,
+    val extension: String
+)
