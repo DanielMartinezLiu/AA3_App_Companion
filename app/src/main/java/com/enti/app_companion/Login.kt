dd732c37b79dfd6e0a1efd5b2dea9418d4e03412
Crami Enti <marc.andrada@enti.cat>
2025-01-10 12:09:03 +0100

    Login and Register working

--- app/src/main/java/com/enti/app_companion/Login.kt
+++ app/src/main/java/com/enti/app_companion/Login.kt
@@ -3,6 +3,7 @@ package com.enti.app_companion
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -13,27 +14,23 @@ import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Query
+import java.sql.Struct
 
 private lateinit var analytics: FirebaseAnalytics
 
 class Login : AppCompatActivity() {
 
-    private  lateinit var mailText : EditText
-    private  lateinit var userText : EditText
-    private  lateinit var passwordText : EditText
-    private  lateinit var database: DatabaseReference
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        mailText = findViewById(R.id.mail_field)
-        userText = findViewById(R.id.username_field)
-        passwordText = findViewById(R.id.password_field)
+        val mailText : EditText  = findViewById(R.id.mail_field)
+        val passwordText : EditText = findViewById(R.id.password_field)
 
         analytics = FirebaseAnalytics.getInstance(this)
-
-        analytics.logEvent("MyFirstEvent", null)
 
         val bundle = Bundle().apply {
             putString("portrait_orentation", (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE).toString())
@@ -42,37 +39,53 @@ class Login : AppCompatActivity() {
         analytics.logEvent("OpenAppSettings", bundle)
 
         val databaseUrl = "https://appcompanion-5f7f6-default-rtdb.europe-west1.firebasedatabase.app"
-        database = FirebaseDatabase.getInstance(databaseUrl).getReference("messages")
-        val dataId = database.push().key
+        database = FirebaseDatabase.getInstance(databaseUrl).getReference("users")
 
         val loginButton: Button = findViewById(R.id.login_button)
         loginButton.setOnClickListener{
-            //Enviar los datos de la base de datos
-            val messageData = mapOf(
-                "user" to "Jose",
-                "message" to "me cago en to sus muertos"
-            )
+            //LOGIN
+            login(mailText.text.toString(), passwordText.text.toString())
 
-            if(dataId != null)
-            {
-                database.child(dataId).setValue(messageData)
-                    .addOnSuccessListener { result ->
-                        Log.d("Firebase Test", "Insert correcto")
-                    }
-                    .addOnFailureListener{ exception ->
-                        Log.d("Firebase Test", "Error: ${exception.message}")
-                    }
-            }
         }
         val registerButton: Button = findViewById(R.id.register_button)
         registerButton.setOnClickListener {
             val intent = Intent(this, Register::class.java)
             startActivity(intent)
         }
-
-
-
-
-
     }
+
+    private  fun login(mailContent : String, passwordContent: String){
+        //Comprobar si los datos son correctos
+        val query: Query = database.orderByChild("mail").equalTo(mailContent)
+
+        query.get()
+            .addOnSuccessListener { snapshot ->
+                Log.d("Login", "Query enviada")
+                if (snapshot.exists()) {
+                    var userFound = false
+                    for (child in snapshot.children) {
+                        val dbPassword = child.child("password").value.toString() // Cambia "password" al nombre exacto del campo en tu base de datos
+                        if (dbPassword == passwordContent) {
+                            userFound = true
+                            break
+                        }
+                    }
+                    if (userFound) {
+                        Log.d("Login", "Usuario y contraseña correctos")
+                        val intent = Intent(this, News::class.java)
+                        startActivity(intent)
+                    } else {
+                        Log.d("Login", "Contraseña incorrecta")
+                    }
+                } else {
+                    Log.d("Login", "Correo no encontrado")
+                }
+
+            }
+            .addOnFailureListener { exception ->
+                Log.e("Login", "Error al consultar la base de datos", exception)
+            }
+    }
+
+
 }
--- app/src/main/java/com/enti/app_companion/Register.kt
+++ app/src/main/java/com/enti/app_companion/Register.kt
@@ -1,20 +1,104 @@
 package com.enti.app_companion
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Query
 
 class Register : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val mailText : EditText = findViewById(R.id.mail_field)
+        val userText : EditText = findViewById(R.id.username_field)
+        val passwordText : EditText = findViewById(R.id.password_field)
+        val repeatedPasswordText : EditText = findViewById(R.id.repeat_password_field)
+
+        val databaseUrl = "https://appcompanion-5f7f6-default-rtdb.europe-west1.firebasedatabase.app"
+        database = FirebaseDatabase.getInstance(databaseUrl).getReference("users")
+        val dataId = database.push().key
+
+        val loginButton: Button = findViewById(R.id.login_button)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
+        val registerButton: Button = findViewById(R.id.register_button)
+        registerButton.setOnClickListener {
+            val mailContent = mailText.text.toString()
+            val userContent = userText.text.toString()
+            val passwordContent = passwordText.text.toString()
+            val repeatedPasswordContent = repeatedPasswordText.text.toString()
+
+            //Si las contraseñas coinciden mostrar error
+            if(passwordContent != repeatedPasswordContent){
+                //Mostrar texto no funca
+                Log.d("Register", "La contraseña no es correcta");
+                Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT)
+                return@setOnClickListener
+            }
+
+            Log.d("Register", "La contraseña es correcta");
+
+            //Si el mail ya existe mostrar error
+            val query: Query = database.orderByChild("mail").equalTo(mailContent)
+
+            query.get()
+                .addOnSuccessListener { snapshot ->
+                    Log.d("Register", "Query enviada")
+                    if(!snapshot.exists()){
+                        Log.d("Register", "El mail no esta en uso");
+                        sendData(dataId, mailContent, userContent, passwordContent)
+                    }
+                    else{
+                        Toast.makeText(this, "El mail esta en uso", Toast.LENGTH_SHORT)
+                        Log.d("Register", "El mail esta en uso");
+                    }
+
+
+                }
+                .addOnFailureListener { exception ->
+                    Log.e("Register", "Error al consultar la base de datos", exception)
+                }
+
+
+
         }
     }
+
+    private fun sendData(dataId : String?, mailContent : String, userContent : String, passwordContent : String){
+        //Enviar los datos de la base de datos
+        val messageData = mapOf(
+            "mail" to mailContent,
+            "user" to userContent,
+            "password" to passwordContent
+        )
+
+        if(dataId != null)
+        {
+            database.child(dataId).setValue(messageData)
+                .addOnSuccessListener { result ->
+                    Log.d("Register", "Registro correcto")
+                    val intent = Intent(this, Login::class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener{ exception ->
+                    Log.d("Register", "Error: ${exception.message}")
+                }
+        }
+    }
+
+
 }
