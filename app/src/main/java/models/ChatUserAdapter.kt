6ad1c8e7f1a4aaeb22a15950349b806dc1c90fe4
Daniel Martinez <daniel.martinez.liu@enti.cat>
2025-01-11 23:17:57 +0100

    User chat working

--- app/src/main/java/models/ChatUserAdapter.kt
+++ app/src/main/java/models/ChatUserAdapter.kt
@@ -2,17 +2,16 @@ package models
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.enti.app_companion.R
 
-class ChatUserAdapter (private val _chats: List<ChatUserModel>, private val onItemClick : (ChatUserModel) -> Unit) : RecyclerView.Adapter<ChatUserAdapter.ChatsUserViewHolder>()
+class ChatUserAdapter (private val _chats: List<String>) : RecyclerView.Adapter<ChatUserAdapter.ChatsUserViewHolder>()
 {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatUserAdapter.ChatsUserViewHolder
     {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_layout_chat_manager, parent, false);
-        return ChatsUserViewHolder(view, onItemClick);
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_layout_user_chat_manager, parent, false);
+        return ChatsUserViewHolder(view);
     }
 
     override fun onBindViewHolder(holder: ChatUserAdapter.ChatsUserViewHolder, position: Int)
@@ -26,14 +25,13 @@ class ChatUserAdapter (private val _chats: List<ChatUserModel>, private val onIt
         return _chats.size;
     }
 
-    class ChatsUserViewHolder(itemView: View, private val onItemClick: (ChatUserModel) -> Unit) : RecyclerView.ViewHolder(itemView)
+    class ChatsUserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     {
-        private val newText : TextView = itemView.findViewById(R.id.new_text);
+        private val newText : TextView = itemView.findViewById(R.id.text);
 
-        fun bind(chatModel: ChatUserModel)
+        fun bind(chatModel: String)
         {
-            newText.text = chatModel.text;
-
+            newText.text = chatModel;
         }
     }
 }
